5d95c082baafd48cf27b7d172d6865df2f78f7dc
ashernamanya <asher@pesamoni.com>
2018-12-06 18:41:20 +0000

    Add gravtar to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  include Gravtastic
+gravtastic
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
